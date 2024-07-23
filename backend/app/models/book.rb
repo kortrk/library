56d463bf53b56d6c4cb19f3bb5425dd43114389c
kortrk <krkortright@gmail.com>
2024-07-23 17:07:18 -0400

    more check_out/check_in improvements to the Book model in the backend

--- backend/app/models/book.rb
+++ backend/app/models/book.rb
@@ -1,10 +1,19 @@
 class Book < ApplicationRecord
   has_many :reviews
 
-  def checkout!(user)
+  BORROWING_PERIOD = 10.days
+
+  def check_out!(user)
+    return nil if self.currentBorrower != nil
     self.currentBorrower = user.id
-    self.duedate = DateTime.now + 10.days
+    self.duedate = DateTime.now + BORROWING_PERIOD
     self.save!
     return duedate
+  end
+
+  def check_in!
+    self.currentBorrower = nil
+    self.duedate = nil
+    self.save!
   end
 end

f59d441b490f267c4c94e69a8b15fa28b953cf2b
kortrk <krkortright@gmail.com>
2024-07-25 15:04:05 -0400

    we need to split the auth info by :, so don't allow it in username or password

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :books, foreign_key: :currentBorrower
-  validates :name, uniqueness: true
+
+  illegal_colon_msg = "  ':' is not an allowed character"
+  validates :name, uniqueness: true, format: { without: /:/, message: illegal_colon_msg }
+  validates :password, format: { without: /:/, message: illegal_colon_msg }
 
   def isLibrarian?
     role == "librarian"

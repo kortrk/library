5b5ffc7e8f4b5bac67687aed0eea62e8a740e28f
kortrk <krkortright@gmail.com>
2024-07-29 16:38:53 -0400

    run npm install again during boot to make the node_modules folder available to the frontend project

--- docker_entry.sh
+++ docker_entry.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 service postgresql start
 
-cd backend
+cd /app/backend
 echo "rake db:create"
 rake db:create
 echo "rake db:migrate"
 rake db:migrate
 echo "rake db:seed"
 rake db:seed
+
+cd /app/frontend
+echo "npm install"
+npm install
 
 cd /app
 
